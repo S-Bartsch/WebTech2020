19f5af56c7c7c5d1af5a556f124e7165754776fd
S-Bartsch <Sabeth.Bartsch@gmail.com>
2021-01-06 15:42:44 +0100

    aufgabe9

--- Uebung6/frontend3/src/app/app.module.ts
+++ Uebung6/frontend3/src/app/app.module.ts
@@ -8,19 +8,28 @@ import { ReadComponent } from './members/read/read.component';
 import { UpdateComponent } from './members/update/update.component';
 import { DeleteComponent } from './members/delete/delete.component';
 import {HttpClientModule} from '@angular/common/http';
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { PencilSquare } from 'ngx-bootstrap-icons';
+import {FormComponent} from "./members/read/form.component";
 
+const icons = {
+  PencilSquare,
+  Trash,
+};
 @NgModule({
-  declarations: [
-    AppComponent,
-    CreateComponent,
-    ReadComponent,
-    UpdateComponent,
-    DeleteComponent
-  ],
+    declarations: [
+        AppComponent,
+        CreateComponent,
+        ReadComponent,
+        UpdateComponent,
+        DeleteComponent,
+        FormComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgxBootstrapIconsModule.pick(icons),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Uebung6/frontend3/src/app/shared/backend.service.ts
+++ Uebung6/frontend3/src/app/shared/backend.service.ts
@@ -12,6 +12,38 @@ export class BackendService {
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Data[]>{
-    return this.http.get<Data[]>(this.baseUrl);
+    return this.http
+      .get<Data[]>(this.baseUrl);
+  }
+
+  getDataById(dataId: number): Observable<Data> {
+    return this.http
+      .get<Data>(this.baseUrl + '/' + dataId);
+  }
+
+  update(dataId: number, data: Data): void {
+    this.http.put<Data>(this.baseUrl + '/' + dataId, data)
+      .subscribe(
+        response => {
+          console.log(response);
+          console.log(response.id);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+
+  deleteOne(dataId: number): void {
+    this.http.delete<Data>(this.baseUrl + '/' + dataId)
+      .subscribe(
+        response => {
+          console.log(response);
+          console.log(response.id);
+        },
+        error => {
+          console.log(error);
+        }
+      );
   }
 }
